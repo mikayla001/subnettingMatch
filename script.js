261c8be60a83d5079debb48bc50579886edb26a2
Mikayla Tate <85713769+mikayla001@users.noreply.github.com>
2025-03-24 17:50:41 -0700

    Create script.js
    javascript for calculate funciont for subnet, the function is not correct yet

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculateSubnet() {
+  const hostCount = document.getElementById('hostCount').value;
+  const ipAddress = document.getElementById('ipAddress').value;
+
+  const result = Math.random();
+
+  document.getElementById('result').textContent = result;
+}
